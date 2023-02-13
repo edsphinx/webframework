8e67690387f63ad01ef45aafa9de2f080127fea4
edsphinx <edsphinx@edsphinxs-MacBook-Pro.local>
2023-02-12 23:43:07 -0600

    Event Listener Logic Added

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import { User } from "./models/User";
 
 const user = new User({ name: 'nyname', age: 20 });
 
-user.set({ age: 58 });
+user.on('change', () => {
+  console.log('change #1');
+});
+user.on('change', () => {
+  console.log('change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('save');
