7b7bf3c5da1b8abaa478a2a2ffadbe19f4ece16f
edsphinx <edsphinx@edsphinxs-MacBook-Pro.local>
2023-02-14 02:56:15 -0600

    Render added to User View

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { User } from "./models/User";
 import { UserForm } from "./Views/UserForm";
 
-const userFrom = new UserForm(document.getElementById('root')!);
+const user = User.buildUser({ name: 'NAME', age: 21 });
+
+const userFrom = new UserForm(document.getElementById('root')!, user);
 
 userFrom.render();
