11d9ffc564df1507c75c2a399bf6fa791555969b
edsphinx <edsphinx@edsphinxs-MacBook-Pro.local>
2023-02-14 03:13:46 -0600

    Event Handling added to User View

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { UserForm } from "./Views/UserForm";
 
 const user = User.buildUser({ name: 'NAME', age: 21 });
 
-const userFrom = new UserForm(document.getElementById('root')!, user);
+const root = document.getElementById('root');
 
-userFrom.render();
+if(root) {
+  const userFrom = new UserForm(root, user);
+  userFrom.render();
+} else {
+  throw new Error('Root element not found');
+}
+
