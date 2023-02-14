3e7eba4c873bc0928c641ce369bc01800a594bc1
edsphinx <edsphinx@edsphinxs-MacBook-Pro.local>
2023-02-14 04:40:42 -0600

    Regions Rendering added to Generic View

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { User } from "./models/User";
-import { UserForm } from "./Views/UserForm";
+import { UserEdit } from "./Views/UserEdit";
 
 const user = User.buildUser({ name: 'NAME', age: 21 });
 
 const root = document.getElementById('root');
 
 if(root) {
-  const userFrom = new UserForm(root, user);
-  userFrom.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found');
 }
